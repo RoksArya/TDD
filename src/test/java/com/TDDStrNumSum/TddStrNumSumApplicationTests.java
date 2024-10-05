10ddf058b76a6e970331e371bf1ee28dafe72010
Rakesh_Arya <0crank0@gmail.com>
2024-10-05 13:22:43 +0530

    Failed: Testing For 2 numbers, no code changes in IMPL

--- src/test/java/com/TDDStrNumSum/TddStrNumSumApplicationTests.java
+++ src/test/java/com/TDDStrNumSum/TddStrNumSumApplicationTests.java
@@ -2,6 +2,8 @@ package com.TDDStrNumSum;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,8 +17,25 @@ class TddStrNumSumApplicationTests {
 	StringSumService strSumService;
 	
 	@Test
-	void contextLoads() {
+	@Order(1)
+	@DisplayName("Test Empty String")
+	void testEmptyStr() {
 		String str = "";
+		assertEquals(0,strSumService.add(str));
+	}
+	
+	@Test
+	@Order(2)
+	@DisplayName("Test One Number")
+	void TestOneNumberStr() {
+		String str = "1";
+		assertEquals(1,strSumService.add(str));
+	}
+	@Test
+	@Order(3)
+	@DisplayName("Test Two Number")
+	void TestTwoNumberStr() {
+		String str = "1,2";
 		assertEquals(0,strSumService.add(str));
 	}
 
